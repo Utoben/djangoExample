060a5ac60ebbd48231dbf95e8334ddeefdaddc75
Utoben <86472697+Utoben@users.noreply.github.com>
2023-01-12 16:44:22 +0500

    Создан toDo list, представление на странице

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks':tasks})
+
+def about(request):
+    return render(request, 'main/about.html')
