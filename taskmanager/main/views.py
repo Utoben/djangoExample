6d4ce83d3f55151f40e4a2cc2dc60658c8c002b9
Utoben <86472697+Utoben@users.noreply.github.com>
2023-01-12 17:45:34 +0500

    Добавлена форма записи задач

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
@@ -10,3 +11,19 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверная'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
